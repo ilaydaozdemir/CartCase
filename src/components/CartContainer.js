edd39f9b470fc9e3d488f9239541d97baa97b4b5
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-06-18 22:04:02 +0300

    Update CartContainer

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux"
-import { store } from '../app/store'
 import CartItem from './CartItem'
 import styled from 'styled-components'
+import axios from 'axios'
+import { initializeItems } from '../features/cartSlice'
 
 const CartContainer = () => {
     const dispatch = useDispatch()
 
+    const getUsers = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users").then(response => {
+            dispatch(initializeItems(response.data))
+        }).catch(error => {
+            console.log("error", error)
+        })
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
     const { cartItems } = useSelector((store) => {
-        console.log("store", store);
-        return store.cart})
-    console.log("cartItems", typeof cartItems, cartItems);
-    return <section className='cartContainer'>
-        <div>    {cartItems.map((item) => {
-            return <CartItem key={item.id}{...item} />
-        })}</div>
+        return store.cart
+    })
 
-    </section>
-
-
+    return (
+        <Container>
+            {cartItems.map((item) => {
+                return <CartItem key={item.id}{...item} />
+            })}
+        </Container>
+    )
 }
+
 export default CartContainer
 
-const cartContainer=styled.section`
+const Container = styled.div`
 display: flex;
-flex-direction: row;
 flex-wrap: wrap;
 `
