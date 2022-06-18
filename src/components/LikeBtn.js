03efa02901a758207efa3502147861150d5cd152
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-06-18 22:02:17 +0300

    Lint component

--- src/components/LikeBtn.js
+++ src/components/LikeBtn.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import styled from "styled-components"
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import styled from 'styled-components'
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined'
 
 const LikeBtn = () => {
-    const [like, setLike] = useState(false);
+    const [like, setLike] = useState(false)
     return (
         <Conatiner onClick={() => setLike((prevLike) => !prevLike)}>
             {like ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
